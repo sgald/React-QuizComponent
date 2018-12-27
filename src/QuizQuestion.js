fe6b2fff5a98ef5ece3464720a1737a7d42668b5
Seamus Gallagher and kieran hegarty <pair+sgald+khegb@allstate.com>
2018-12-27 10:55:31 +0000

    Quiz Component created

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,16 +4,14 @@ class QuizQuestion extends Component {
 
     render(){
 
-        let quiz_question = this.props.quiz_question;
-
        return(
            <main>
                 <section>
-                    <p>{quiz_question.instruction_text}</p>
+                    <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
                     <ul>
-                        <li>{quiz_question.answer_options[0]}</li>
+                        <li>{this.props.quiz_question.answer_options[0]}</li>
                     </ul>
                 </section>
             </main>
